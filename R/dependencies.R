344d4dddfd98cacc744adfc407152c65383e3509
Malte Grosser <malte.grosser@gmail.com>
2025-07-29 16:50:07 +0200

    update

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,11 +1,18 @@
+#' SurveyJS core HTML dependency
+#'
+#' Internal helper that injects the SurveyJS core library as an
+#' `htmltools::htmlDependency` for your widget.
+#'
+#' @return An [htmltools::htmlDependency] for SurveyJS v2.2.6
+#' @importFrom htmltools htmlDependency
 #' @keywords internal
 dep_surveyjs_core <- function() {
   htmltools::htmlDependency(
-    name    = "surveyjs-v2",
-    version = "2.2.6",
-    src     = "htmlwidgets/lib/surveyjs/2.2.6",
-    script  = c("survey.core.min.js", "survey-react-ui.min.js"),  # ← updated
+    name       = "surveyjs-v2",
+    version    = "2.2.6",
+    src        = "htmlwidgets/lib/surveyjs/2.2.6",
+    script     = c("survey.core.min.js", "survey-react-ui.min.js"),
     stylesheet = "survey-core.min.css",
-    package = "rsurveyjs"
+    package    = "rsurveyjs"
   )
 }
